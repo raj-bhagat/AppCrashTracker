8ea57df2df54ff7dfc886906e0bdd2dd9f3ccc09
raj-bhagat <rajkrbhagat911@gmail.com>
2021-10-20 17:11:22 +0530

    porting done
    This library has been ported and static tool testing done

---
+++ AppCrashTracker/appcrashtracker/src/main/java/com/org/appcrashtracker/PostValuesPair.java
@@ -0,0 +1,21 @@
+package com.org.appcrashtracker;
+
+public class PostValuesPair {
+
+	String key="",value="";
+
+	public PostValuesPair(String key,String value) {
+		this.key=key;
+		this.value=value;
+	}
+
+	public String getKey()
+	{
+		return this.key;
+	}
+
+	public String getValue()
+	{
+		return this.value;
+	}
+}
---
+++ AppCrashTracker/entry/src/main/java/com/example/appcrashtracker/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.appcrashtracker;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
